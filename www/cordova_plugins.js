4d6e714a0832cc505f09cb4cf79373b68f7935c7
Manuel Martinez <mmartenez@mutua.local>
2021-02-15 16:55:32 +0100

    Añadido proyecto iOS

---
+++ www/cordova_plugins.js
@@ -0,0 +1,87 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-appcenter-shared.AppCenter",
+      "file": "plugins/cordova-plugin-appcenter-shared/www/AppCenter.js",
+      "pluginId": "cordova-plugin-appcenter-shared",
+      "clobbers": [
+        "AppCenter"
+      ]
+    },
+    {
+      "id": "cordova-plugin-appcenter-analytics.Analytics",
+      "file": "plugins/cordova-plugin-appcenter-analytics/www/Analytics.js",
+      "pluginId": "cordova-plugin-appcenter-analytics",
+      "clobbers": [
+        "AppCenter.Analytics"
+      ]
+    },
+    {
+      "id": "cordova-plugin-appcenter-crashes.Crashes",
+      "file": "plugins/cordova-plugin-appcenter-crashes/www/Crashes.js",
+      "pluginId": "cordova-plugin-appcenter-crashes",
+      "clobbers": [
+        "AppCenter.Crashes"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-keyboard.keyboard",
+      "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+      "pluginId": "cordova-plugin-ionic-keyboard",
+      "clobbers": [
+        "window.Keyboard"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.IonicWebView",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "Ionic.WebView"
+      ]
+    },
+    {
+      "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-ionic-webview",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-appcenter-shared": "0.5.1",
+    "cordova-plugin-appcenter-analytics": "0.5.1",
+    "cordova-plugin-appcenter-crashes": "0.5.1",
+    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-ionic-keyboard": "2.2.0",
+    "cordova-plugin-ionic-webview": "4.2.1",
+    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.3"
+  };
+});
